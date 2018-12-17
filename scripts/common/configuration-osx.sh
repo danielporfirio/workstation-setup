89a775898ea09ffe59fb095b1e7db3d33b13f372
Daniel Porfirio <me@danielporfirio.com>
2018-12-17 10:53:48 -0200

    settings default dock apps

--- scripts/common/configuration-osx.sh
+++ scripts/common/configuration-osx.sh
@@ -6,12 +6,19 @@ echo 'Customizing OS X configuration'
 defaults write com.apple.menuextra.clock "DateFormat" 'EEE MMM d  h:mm:ss a'
 killall SystemUIServer
 
+
 # modify appearance of dock: remove standard icons, add chrome and iTerm
 curl https://raw.githubusercontent.com/kcrawford/dockutil/master/scripts/dockutil > /usr/local/bin/dockutil
 chmod a+rx,go-w /usr/local/bin/dockutil
 dockutil --list | awk -F\t '{print "dockutil --remove \""$1"\" --no-restart"}' | sh
-dockutil --add /Applications/Google\ Chrome.app --no-restart
-dockutil --add /Applications/iTerm.app
+
+for app in "Notes" \
+	"Spotify" \
+	"Google Chrome" \
+	"iTerm" ; do
+	dockutil --add "/Applications/${app}.app"
+done
+
 
  
  
